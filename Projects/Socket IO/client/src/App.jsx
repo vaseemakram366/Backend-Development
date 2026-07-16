7ab79e6820ac0a981243674629279a9eeb757cfd
Vaseem Akram <vaseemakrama366@gmail.com>
2026-07-16 23:17:46 +0530

    Completed Socket.io in detail

--- Projects/Socket IO/client/src/App.jsx
+++ Projects/Socket IO/client/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
 import { io } from "socket.io-client"
 const socket = io("http://localhost:3000")
@@ -6,30 +6,112 @@ const socket = io("http://localhost:3000")
 function App() {
 
 
+
+  const [message, setMessage] = useState("")
+  const [room, setRoom] = useState("")
+  const [messageHistory, setMessageHistory] = useState([])
+  const [socketID, setSocketID] = useState("")
+  const [roomName, setRoomName] = useState("")
+  const [joinMessage, setJoinMessage] = useState("")
+
+  const handleRoomChat = (e) => {
+    e.preventDefault();
+
+    socket.emit("join-room", roomName)
+    setRoomName("")
+
+
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    // socket.emit("user-to-all",message)
+
+    socket.emit("individual-message", { message, room })
+    setMessage("")
+    setRoom("")
+  }
+
   useEffect(() => {
 
     socket.on("connect", () => {
       console.log("User Connected :", socket.id)
+      setSocketID(socket.id)
     })
 
-    socket.on("message", (msg) => {
+    // socket.on("message",(msg)=>{
+    //   console.log(msg)
+    // })
+
+    // socket.on("sendingtoall",(data)=>{
+    //   console.log(data)
+    // })
+
+    //  socket.emit("sendingtoallfrontend","Hello for all (Frontend)")
+
+    //  socket.on("receive-message",(data)=>{
+    //   console.log("frontend:",data)
+    //  })
+
+
+    //  socket.on("server-to-all",(msg)=>{
+    //   console.log(msg)
+    //  })
+
+
+    socket.on("server-to-individual", (msg) => {
       console.log(msg)
+
+      setMessageHistory((prev) => ([...prev, msg]))
     })
 
-    socket.on("sendingtoall", (data) => {
-      console.log(data)
+    socket.on("joining", (msg) => {
+      setJoinMessage(msg)
     })
 
-    socket.emit("sendingtoallfrontend", "Hello for all (Frontend)")
+    return () => {
+      socket.disconnect()
+    }
 
-    socket.on("receive-message", (data) => {
-      console.log("frontend:", data)
-    })
 
 
   }, [])
   return (
-    <div>App</div>
+    <div>
+
+      <h3>{socketID}</h3>
+
+      <form onSubmit={handleRoomChat}>
+
+        <input type="text" placeholder='Room name' value={roomName} onChange={(e) => setRoomName(e.target.value)} />
+
+
+        <button type='submit'>Join Room</button>
+      </form>
+
+      <br />
+
+
+      <form onSubmit={handleSubmit}>
+
+        <input type="text" placeholder='Write a message' value={message} onChange={(e) => setMessage(e.target.value)} />
+
+        <input type="text" placeholder='Enter room ID' value={room} onChange={(e) => setRoom(e.target.value)} />
+        <button type='submit'>Send</button>
+      </form>
+
+
+      <div>
+
+        {messageHistory.map((item, index) => {
+          return <p key={index}>{item}</p>
+        })}
+
+      </div>
+
+      {joinMessage && <h3>{joinMessage}</h3>}
+    </div>
   )
 }
 
--- Projects/Socket IO/server/index.js
+++ Projects/Socket IO/server/index.js
@@ -9,11 +9,11 @@ const server = createServer(app)
 
 const io = new Server(server,{
     cors:{
-        origin:"http://localhost:5174"
+        origin:"http://localhost:5173"
     }
 })
 
-
+// to create a socket io connection
 io.on("connection",(socket)=>{
     console.log("User Connected:",socket.id)
 
@@ -23,6 +23,37 @@ io.on("connection",(socket)=>{
 
     socket.on("sendingtoallfrontend",(msg)=>{
         io.emit("receive-message",msg)
+    })
+
+// // to receive "to all" invoked message  and send messages to all
+    // socket.on("user-to-all",(msg)=>{
+    //     // io.emit("server-to-all",msg) // sending message to all including that sender
+    
+    //     socket.broadcast.emit("server-to-all",msg) //sending message to all excluding that sender
+    // })
+
+
+    // to receive invoked message and send messages to specifc person
+
+    socket.on("individual-message",({message,room})=>{
+
+        io.to(room).emit("server-to-individual",message) // to send messages  to specifc person
+
+    })
+
+
+    //to get the room name or id and send has joined message
+    socket.on("join-room",(roomName)=>{
+        socket.join(roomName)
+
+        io.to(roomName).emit("joining",`${socket.id} has joined this group`)
+    })
+
+
+    //disconnection
+
+    socket.on("disconnect",()=>{
+        console.log("user disconnected : ",socket.id)
     })
 })
 
