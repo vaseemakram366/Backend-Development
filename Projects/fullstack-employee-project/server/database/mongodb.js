9b353d21e871ada3e0331a6be9802313fc3f5f42
Vaseem Akram <vaseemakrama366@gmail.com>
2026-07-20 11:11:29 +0530

    deployment added

--- Projects/fullstack-employee-project/server/database/mongodb.js
+++ Projects/fullstack-employee-project/server/database/mongodb.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 async function connectDb(){
     try {
-        await mongoose.connect('mongodb://localhost:27017/employeeSystem')
+        await mongoose.connect('mongodb://fullstackDatabase:Vaseem1234@ac-jqoqthg-shard-00-00.ltgn4oq.mongodb.net:27017,ac-jqoqthg-shard-00-01.ltgn4oq.mongodb.net:27017,ac-jqoqthg-shard-00-02.ltgn4oq.mongodb.net:27017/?ssl=true&replicaSet=atlas-9lxald-shard-0&authSource=admin&appName=Cluster0')
         console.log('database connected successfully...');
         
     } catch (error){
